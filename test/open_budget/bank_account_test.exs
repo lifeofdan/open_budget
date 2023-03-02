e79b81c1996da4e3820f04cdfc000fd1d6c8b720
Daniel Newman <contact@lifeofdan.me>
2023-03-02 21:36:54 +1000

    Add read and destroy tests

--- test/open_budget/bank_account_test.exs
+++ test/open_budget/bank_account_test.exs
@@ -151,4 +151,99 @@ defmodule OpenBudget.BankAccountTest do
 
     assert response_ok == :ok
   end
+
+  test "can only read account if is your budget" do
+    user =
+      User
+      |> Ash.Changeset.for_create(:register_with_password, %{
+        email: "test@user.com",
+        hashed_password: "password",
+        password: "password",
+        password_confirmation: "password"
+      })
+      |> OpenBudget.Accounts.create!()
+
+    budget =
+      Budget
+      |> Ash.Changeset.for_create(:new_budget, %{title: "My new budget", active: true},
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    OpenBudget.Budgets.BankAccount
+    |> Ash.Changeset.for_create(
+      :create_bank_account,
+      %{
+        title: "My new account",
+        budget_id: budget.id
+      },
+      actor: user
+    )
+    |> OpenBudget.Budgets.create!()
+
+    no_budget_read_account =
+      OpenBudget.Budgets.BankAccount
+      |> Ash.Query.for_read(:read, %{})
+      |> OpenBudget.Budgets.read()
+
+    {no_budget_read_response, _no_budget_read_response} = no_budget_read_account
+
+    assert no_budget_read_response == :error
+
+    read_account =
+      OpenBudget.Budgets.BankAccount
+      |> Ash.Query.for_read(:read, %{}, actor: budget)
+      |> OpenBudget.Budgets.read()
+
+    {read_account_response, _read_account_result} = read_account
+
+    assert read_account_response == :ok
+  end
+
+  test "can destroy with budget, cannot destroy without budget" do
+    user =
+      User
+      |> Ash.Changeset.for_create(:register_with_password, %{
+        email: "test@user.com",
+        hashed_password: "password",
+        password: "password",
+        password_confirmation: "password"
+      })
+      |> OpenBudget.Accounts.create!()
+
+    budget =
+      Budget
+      |> Ash.Changeset.for_create(:new_budget, %{title: "My new budget", active: true},
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    account =
+      OpenBudget.Budgets.BankAccount
+      |> Ash.Changeset.for_create(
+        :create_bank_account,
+        %{
+          title: "My new account",
+          budget_id: budget.id
+        },
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    cannot_destroy =
+      account
+      |> Ash.Changeset.for_destroy(:destroy)
+      |> OpenBudget.Budgets.destroy()
+
+    {cannot_destroy_response, _cannot_destroy_result} = cannot_destroy
+
+    assert cannot_destroy_response == :error
+
+    can_destroy =
+      account
+      |> Ash.Changeset.for_destroy(:destroy, %{}, actor: budget)
+      |> OpenBudget.Budgets.destroy()
+
+    assert can_destroy == :ok
+  end
 end
