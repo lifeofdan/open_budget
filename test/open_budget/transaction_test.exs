15016011774aa3b09b096c6a3a20d17e0cc8ddad
Daniel Newman <contact@lifeofdan.me>
2023-03-14 10:12:23 +1000

    Add transaction test cases

--- test/open_budget/transaction_test.exs
+++ test/open_budget/transaction_test.exs
@@ -45,7 +45,7 @@ defmodule OpenBudget.TransactionTest do
       )
       |> OpenBudget.Budgets.create()
 
-    {response, result} = transaction
+    {response, _result} = transaction
 
     assert response == :ok
   end
@@ -121,17 +121,16 @@ defmodule OpenBudget.TransactionTest do
       )
       |> OpenBudget.Budgets.create!()
 
-    bank_account =
-      OpenBudget.Budgets.BankAccount
-      |> Ash.Changeset.for_create(
-        :create_bank_account,
-        %{
-          title: "My new account",
-          budget_id: budget.id
-        },
-        actor: user
-      )
-      |> OpenBudget.Budgets.create!()
+    OpenBudget.Budgets.BankAccount
+    |> Ash.Changeset.for_create(
+      :create_bank_account,
+      %{
+        title: "My new account",
+        budget_id: budget.id
+      },
+      actor: user
+    )
+    |> OpenBudget.Budgets.create!()
 
     transaction =
       OpenBudget.Budgets.Transaction
@@ -276,5 +275,114 @@ defmodule OpenBudget.TransactionTest do
     [%{class: invalid_actor_error_class}] = invalid_actor_errors
 
     assert invalid_actor_error_class == :invalid
+  end
+
+  test "can read transaction" do
+    user =
+      OpenBudget.Accounts.User
+      |> Ash.Changeset.for_create(:register_with_password, %{
+        email: "test@user.com",
+        hashed_password: "password",
+        password: "password",
+        password_confirmation: "password"
+      })
+      |> OpenBudget.Accounts.create!()
+
+    budget =
+      OpenBudget.Budgets.Budget
+      |> Ash.Changeset.for_create(:new_budget, %{title: "My new budget", active: true},
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    bank_account =
+      OpenBudget.Budgets.BankAccount
+      |> Ash.Changeset.for_create(
+        :create_bank_account,
+        %{
+          title: "My new account",
+          budget_id: budget.id
+        },
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    OpenBudget.Budgets.Transaction
+    |> Ash.Changeset.for_create(
+      :create_transaction,
+      %{
+        title: "New transaction",
+        amount: -14.53,
+        bank_account_id: bank_account.id
+      },
+      actor: budget
+    )
+    |> OpenBudget.Budgets.create!()
+
+    read_transaction =
+      OpenBudget.Budgets.Transaction
+      |> Ash.Query.for_read(:read, %{}, actor: bank_account)
+      |> OpenBudget.Budgets.read()
+
+    {response, _result} = read_transaction
+    assert response == :ok
+  end
+
+  test "cannot read data with invalid actor" do
+    user =
+      OpenBudget.Accounts.User
+      |> Ash.Changeset.for_create(:register_with_password, %{
+        email: "test@user.com",
+        hashed_password: "password",
+        password: "password",
+        password_confirmation: "password"
+      })
+      |> OpenBudget.Accounts.create!()
+
+    budget =
+      OpenBudget.Budgets.Budget
+      |> Ash.Changeset.for_create(:new_budget, %{title: "My new budget", active: true},
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    bank_account =
+      OpenBudget.Budgets.BankAccount
+      |> Ash.Changeset.for_create(
+        :create_bank_account,
+        %{
+          title: "My new account",
+          budget_id: budget.id
+        },
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    OpenBudget.Budgets.Transaction
+    |> Ash.Changeset.for_create(
+      :create_transaction,
+      %{
+        title: "New transaction",
+        amount: -14.53,
+        bank_account_id: bank_account.id
+      },
+      actor: budget
+    )
+    |> OpenBudget.Budgets.create!()
+
+    read_transaction_empty =
+      OpenBudget.Budgets.Transaction
+      # user is the wrong thing to pass
+      |> Ash.Query.for_read(:read, %{}, actor: user)
+      |> OpenBudget.Budgets.read!()
+
+    assert read_transaction_empty == []
+
+    read_transaction_ok =
+      OpenBudget.Budgets.Transaction
+      |> Ash.Query.for_read(:read, %{}, actor: bank_account)
+      |> OpenBudget.Budgets.read!()
+
+    assert length(read_transaction_ok) != 0
   end
 end
