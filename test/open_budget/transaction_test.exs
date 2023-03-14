bb63f47d0ebc0790142d6d62bcb2d00ede36aea8
Daniel Newman <contact@lifeofdan.me>
2023-03-14 21:54:06 +1000

    Add destroy testing for transactions

--- test/open_budget/transaction_test.exs
+++ test/open_budget/transaction_test.exs
@@ -770,4 +770,186 @@ defmodule OpenBudget.TransactionTest do
     assert response == :error
     assert result_error_class == :invalid
   end
+
+  test "can destroy transaction" do
+    user =
+      OpenBudget.Accounts.User
+      |> Ash.Changeset.for_create(:register_with_password, %{
+        email: "test@user.com",
+        hashed_password: "password",
+        password: "password",
+        password_confirmation: "password"
+      })
+      |> OpenBudget.Accounts.create!()
+
+    budget =
+      OpenBudget.Budgets.Budget
+      |> Ash.Changeset.for_create(:new_budget, %{title: "My new budget", active: true},
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    bank_account =
+      OpenBudget.Budgets.BankAccount
+      |> Ash.Changeset.for_create(
+        :create_bank_account,
+        %{
+          title: "My new account",
+          budget_id: budget.id
+        },
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    transaction =
+      OpenBudget.Budgets.Transaction
+      |> Ash.Changeset.for_create(
+        :create_transaction,
+        %{
+          title: "New transaction",
+          amount: -14.53,
+          bank_account_id: bank_account.id
+        },
+        actor: budget
+      )
+      |> OpenBudget.Budgets.create!()
+
+    read_transaction =
+      OpenBudget.Budgets.Transaction.get_by_id!(transaction.id, actor: bank_account)
+
+    assert read_transaction.title == "New transaction"
+
+    read_transaction
+    |> Ash.Changeset.for_destroy(:destroy, %{id: transaction.id}, actor: bank_account)
+    |> OpenBudget.Budgets.destroy!()
+
+    read_after_destroyed =
+      OpenBudget.Budgets.Transaction.get_by_id(transaction.id, actor: bank_account)
+
+    {response, result} = read_after_destroyed
+    %{class: result_class} = result
+
+    assert response == :error
+    assert result_class == :invalid
+  end
+
+  test "cannot destroy without actor" do
+    user =
+      OpenBudget.Accounts.User
+      |> Ash.Changeset.for_create(:register_with_password, %{
+        email: "test@user.com",
+        hashed_password: "password",
+        password: "password",
+        password_confirmation: "password"
+      })
+      |> OpenBudget.Accounts.create!()
+
+    budget =
+      OpenBudget.Budgets.Budget
+      |> Ash.Changeset.for_create(:new_budget, %{title: "My new budget", active: true},
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    bank_account =
+      OpenBudget.Budgets.BankAccount
+      |> Ash.Changeset.for_create(
+        :create_bank_account,
+        %{
+          title: "My new account",
+          budget_id: budget.id
+        },
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    transaction =
+      OpenBudget.Budgets.Transaction
+      |> Ash.Changeset.for_create(
+        :create_transaction,
+        %{
+          title: "New transaction",
+          amount: -14.53,
+          bank_account_id: bank_account.id
+        },
+        actor: budget
+      )
+      |> OpenBudget.Budgets.create!()
+
+    read_transaction =
+      OpenBudget.Budgets.Transaction.get_by_id!(transaction.id, actor: bank_account)
+
+    assert read_transaction.title == "New transaction"
+
+    try_destroy =
+      read_transaction
+      |> Ash.Changeset.for_destroy(:destroy, %{id: transaction.id})
+      |> OpenBudget.Budgets.destroy()
+
+    {response, result} = try_destroy
+    %{errors: [%{class: result_error_class}]} = result
+
+    assert response == :error
+    assert result_error_class == :forbidden
+  end
+
+  test "cannot destroy with invalid actor" do
+    user =
+      OpenBudget.Accounts.User
+      |> Ash.Changeset.for_create(:register_with_password, %{
+        email: "test@user.com",
+        hashed_password: "password",
+        password: "password",
+        password_confirmation: "password"
+      })
+      |> OpenBudget.Accounts.create!()
+
+    budget =
+      OpenBudget.Budgets.Budget
+      |> Ash.Changeset.for_create(:new_budget, %{title: "My new budget", active: true},
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    bank_account =
+      OpenBudget.Budgets.BankAccount
+      |> Ash.Changeset.for_create(
+        :create_bank_account,
+        %{
+          title: "My new account",
+          budget_id: budget.id
+        },
+        actor: user
+      )
+      |> OpenBudget.Budgets.create!()
+
+    transaction =
+      OpenBudget.Budgets.Transaction
+      |> Ash.Changeset.for_create(
+        :create_transaction,
+        %{
+          title: "New transaction",
+          amount: -14.53,
+          bank_account_id: bank_account.id
+        },
+        actor: budget
+      )
+      |> OpenBudget.Budgets.create!()
+
+    read_transaction =
+      OpenBudget.Budgets.Transaction.get_by_id!(transaction.id, actor: bank_account)
+
+    assert read_transaction.title == "New transaction"
+
+    try_destroy =
+      read_transaction
+      |> Ash.Changeset.for_destroy(:destroy, %{id: transaction.id}, actor: budget)
+      |> OpenBudget.Budgets.destroy()
+
+    {response, result} = try_destroy
+    %{errors: [%{class: result_error_class}]} = result
+
+    assert response == :error
+    assert result_error_class == :forbidden
+  end
 end
